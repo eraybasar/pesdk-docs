80218f71ff5ef568a051a40cb556a6b235809e76
Sascha Brink <sascha.brink@gmail.com>
2019-09-24 12:08:04 +0200

    Revert "Speed up site generation"
    This reverts commit bb82d0af1e7d1a7e4c9798ae25b9ade889e82728.

--- Gemfile
+++ Gemfile
@@ -2,10 +2,5 @@ source 'https://rubygems.org'
 
 require 'json'
 require 'open-uri'
-gem 'irb', require: false
 
-gem 'jekyll', '~> 4.0.0'
-gem "jekyll-sitemap"
-gem "jekyll-redirect-from"
-gem "jekyll-gist"
-gem 'fileutils'
+gem 'github-pages'
--- _plugins/copy_static_apidocs.rb
+++
@@ -1,9 +0,0 @@
-require 'fileutils'
-
-Jekyll::Hooks.register :site, :post_write do |p|
-  puts "Copying static api docs"
-  Dir.glob('_apidocs/android/v*').each { |d| FileUtils.cp_r d, '_site/apidocs/android/' }
-  Dir.glob('_apidocs/ios/v*').each { |d| FileUtils.cp_r d, '_site/apidocs/ios/' }
-  Dir.glob('_apidocs/html5/v*').each { |d| FileUtils.cp_r d, '_site/apidocs/html5/' }
-  Dir.glob('_apidocs/server/v*').each { |d| FileUtils.cp_r d, '_site/apidocs/server/' }
-end
