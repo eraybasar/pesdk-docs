d9ca19e3b02c0bf70701fe2ca372af43922d0289
Alexander Schiewe <alexander.schiewe@img.ly>
2019-09-25 09:24:50 +0200

    Symlink API docs instead of copying

--- _plugins/copy_static_apidocs.rb
+++ _plugins/copy_static_apidocs.rb
@@ -1,9 +1,10 @@
 require 'fileutils'
 
-Jekyll::Hooks.register :site, :post_write do |p|
-  puts "Copying static api docs"
-  Dir.glob('_apidocs/android/v*').each { |d| FileUtils.cp_r d, '_site/apidocs/android/' }
-  Dir.glob('_apidocs/ios/v*').each { |d| FileUtils.cp_r d, '_site/apidocs/ios/' }
-  Dir.glob('_apidocs/html5/v*').each { |d| FileUtils.cp_r d, '_site/apidocs/html5/' }
-  Dir.glob('_apidocs/server/v*').each { |d| FileUtils.cp_r d, '_site/apidocs/server/' }
+Jekyll::Hooks.register :site, :post_write do |site|
+  puts "Symlinking static API docs"
+  ["android", "ios", "html5", "server"].each { |platform|
+    FileUtils.cd("#{site.dest}/apidocs/#{platform}/") do
+      FileUtils.ln_s Dir.glob("#{site.source}/_apidocs/#{platform}/v*"), "."
+    end
+  }
 end
